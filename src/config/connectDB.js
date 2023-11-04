812fd6479ef4f063050f2d8fd99eaa832d3404a6
Ho Viet Dung <vietdung04940@gmail.com>
2023-11-04 15:09:29 +0900

    uodate

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("VietDung", "root", null, {
+const sequelize = new Sequelize("dung", "root", null, {
   host: "localhost",
   dialect: "mysql",
 });
