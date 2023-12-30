162d936381c232b5f28165da788354c698d786fa
Ho Viet Dung <vietdung04940@gmail.com>
2023-12-30 17:01:47 +0900

    new update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,35 +1,47 @@
 import db from "../models/index";
 import CRUDservice from "../services/CRUDservice";
 
+//open Home page
 let getHomePage = async (req, res) => {
   try {
-    let data = await db.User.findAll();
+    // let data = await db.User.findAll();
     return res.render("homepage.ejs", {
-      data: JSON.stringify(data),
+      // data: JSON.stringify(data),
+      title: "Home page from homeController",
     });
   } catch (e) {
     console.log(e);
   }
 };
 
+// open About page
 let getAboutPage = (req, res) => {
-  return res.render("test/about.ejs");
+  return res.render("test/about.ejs", {
+    title: "About Page",
+  });
 };
 
-let getCRUD = (req, res) => {
-  res.render("crud.ejs");
+//open Add new user page
+let addCrud = (req, res) => {
+  res.render("addCrud.ejs", { title: "Add CRUD" });
 };
 
+//tao user moi tu trang
 let postCrud = async (req, res) => {
-  let mess = await CRUDservice.createNewUser(req.body);
-  console.log(mess);
-  res.send("hihihehe");
+  await CRUDservice.createNewUser(req.body);
+  const data = await CRUDservice.getAllUser();
+  return res.render("displayCRUD", {
+    //sau khi them user thi lai render(chuyen huong sang displayCRUD) ra tat ca user
+    data: data,
+    title: "Display all user",
+  });
 };
 
 let displayCRUD = async (req, res) => {
   const data = await CRUDservice.getAllUser();
   return res.render("displayCRUD", {
     data: data,
+    title: "Display all user",
   });
 };
 
@@ -37,17 +49,26 @@ let editCrud = async (req, res) => {
   let userId = req.query.id;
   if (userId) {
     let userInfo = await CRUDservice.getUserInfo(userId);
-    console.log("user info from homeController.js\n", userInfo);
-    return res.send("found an user");
+    // console.log("user info from homeController.js > editCrud \n", userInfo);
+    // return res.send("found an user: " + userInfo.firstName);
+    return res.render("editUser.ejs", {
+      title: "Edit User",
+      userInfo: userInfo,
+    });
   }
   return res.send("found no user");
+};
+
+let updateCrud = (req, res) => {
+  res.send("updateCrud page kara");
 };
 
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
-  getCRUD: getCRUD,
+  addCrud: addCrud,
   postCrud: postCrud,
   displayCRUD: displayCRUD,
   editCrud: editCrud,
+  updateCrud: updateCrud,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -6,10 +6,11 @@ let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
-  router.get("/getCRUD", homeController.getCRUD);
+  router.get("/addCrud", homeController.addCrud);
   router.post("/post-crud", homeController.postCrud);
   router.get("/displayCRUD", homeController.displayCRUD);
   router.get("/editCrud", homeController.editCrud);
+  router.post("/updateCrud", homeController.updateCrud);
 
   return app.use("/", router);
 };
